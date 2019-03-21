7d042f01df9754a776658fdc9a1ffea9aefd572e
hy9125 <hy9125@arcsoft.com.cn>
2019-03-21 16:28:02 +0800

    首页样例，host列表

--- sb_cloud_ui/src/views/home/Home.vue
+++ sb_cloud_ui/src/views/home/Home.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>这是首页</div>
+  <div>
+    <el-card style="border-radius: 0">
+      <div slot="header">
+        <span>概况</span>
+      </div>
+      <div class="demo">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="grid-content">
+              <div style="padding: 10px;background: #673AB7">
+                <i class="el-icon-location-outline index-icon"></i>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content ">
+              <div style="padding: 10px;background: #3c8dbc">
+                <i class="el-icon-picture index-icon"></i>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content ">
+              <div style="padding: 10px;background: #009688">
+                <i class="el-icon-service index-icon"></i>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content ">
+              <div style="padding: 10px;background: #607D8B">
+                <i class="el-icon-bell index-icon"></i>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
+    <br>
+  </div>
 
 </template>
 
--- sb_cloud_ui/src/views/host/HostList.vue
+++ sb_cloud_ui/src/views/host/HostList.vue
@@ -16,7 +16,60 @@
 
         </el-row>
       </el-header>
-      <div>主机列表</div>
+      <el-table
+        :data="usersData"
+        ref="table"
+        style="width: 100%">
+        <el-table-column
+          prop="loginname"
+          label="登录账号">
+        </el-table-column>
+        <el-table-column
+          prop="nickname"
+          label="昵称">
+        </el-table-column>
+        <el-table-column
+          prop="email"
+          label="邮箱">
+        </el-table-column>
+        <el-table-column
+          prop="cellphone"
+          label="手机">
+        </el-table-column>
+        <el-table-column
+          prop="result"
+          label="用户体质">
+          <div slot-scope="scope" style="width: 100%;text-align: center">{{ scope.row.result ?
+            $Config.tizhiCategories[scope.row.result]:'未判定' }}
+          </div>
+        </el-table-column>
+        <el-table-column
+          prop="sex"
+          width="66"
+          label="性别">
+          <div slot-scope="scope" style="width: 100%;text-align: center">{{ $Config.sex[scope.row.sex] }}</div>
+        </el-table-column>
+        <el-table-column
+          prop="active"
+          width="100"
+          label="是否激活">
+          <div slot-scope="scope" style="width: 100%;text-align: center">
+            <el-tag v-if="scope.row.active">正常</el-tag>
+            <el-tag v-else type="danger">被删除</el-tag>
+          </div>
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="180">
+          <template slot-scope="scope">
+            <el-button @click="" type="warning" style="transition: .4s;"  :ref="scope.row.id"  icon="el-icon-refresh" size="small" circle></el-button>
+            <el-button @click="" type="primary" icon="el-icon-edit" size="small" circle></el-button>
+            <!--<el-button @click="deleteUser(scope.row.id)" v-if="scope.row.active != '0'" type="danger" icon="el-icon-delete" circle size="small"></el-button>-->
+            <!--<el-button @click="deleteUser(scope.row.id)" v-else icon="el-icon-check" circle size="small"></el-button>-->
+          </template>
+        </el-table-column>
+      </el-table>
     </el-container>
 
   </div>
@@ -30,6 +83,16 @@
     data() {
       return {
         firm_id: '',
+        params: {
+          name: '',
+        },
+        usersData: [
+          {id:1,loginname:'Admin',nickname:'管理员',email:'Admin@.admin.com',cellphone:'151178xxxx',sex:'male',active:1},
+          {id:2,loginname:'SenLin',nickname:'森林',email:'SenLin@.admin.com',cellphone:'151178xxxx',sex:'unknown',active:0},
+          {id:4,loginname:'Admin1',nickname:'赵晓',email:'Admin@.admin.com',cellphone:'151178xxxx',sex:'male',active:1},
+          {id:5,loginname:'Wujun',nickname:'吴军',email:'Admin@.admin.com',cellphone:'151178xxxx',sex:'male',active:1},
+          {id:5,loginname:'Huang',nickname:'黄家',email:'Admin@.admin.com',cellphone:'151178xxxx',sex:'male',active:1},
+        ]
       }
     },
     components: {}
