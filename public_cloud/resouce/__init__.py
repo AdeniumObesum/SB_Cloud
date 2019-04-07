97b825035331cf4e7d392269bda8dbc2ccf2f876
Clare <1837866781@qq.com>
2019-04-07 19:20:04 +0800

    目录分层

---
+++ public_cloud/resouce/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : __init__.py.py
+# @Author: Clare
+# @Date  : 2019/4/5 
+# @license : Copyright(C), Nanyang Institute of Technology 
+# @Contact : 1837866781@qq.com 
+# @Software : PyCharm
+# code is far away from bugs with the god animal protecting
+__pet__ = '''   
+              ┏┓     ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+'''
--- public_cloud/urls.py
+++ public_cloud/urls.py
@@ -10,21 +10,22 @@
 
 from django.urls import path
 
-from public_cloud.account import views
+from public_cloud.account import views as account_views
+from public_cloud.resouce import views as resouce_views
 
 urlpatterns = [
     # path('public_cloud/', include('public_cloud.urls')),
-    path('create_family/', views.CreateFamily.as_view()),
-    path('get_families/', views.GetFamily.as_view()),
-    path('get_accounts/', views.GetAccount.as_view()),
-    path('add_account/', views.AddAccount.as_view()),
-    path('get_firms/', views.GetFirm.as_view()),
-    path('get_account_detail/', views.GetAccountDetail.as_view()),
-    path('import_host/', views.ImportHost.as_view()),
-    path('get_family_firms/', views.GetFamilyFirm.as_view()),
-    path('get_hosts/', views.GetHost.as_view()),
-    path('stop_instance/', views.StopInstance.as_view()),
-    path('start_instance/', views.StartInstance.as_view()),
-    path('get_disks/', views.GetDisk.as_view()),
-    path('get_snapshots/', views.GetSnapshot.as_view()),
+    path('create_family/', account_views.CreateFamily.as_view()),
+    path('get_families/', account_views.GetFamily.as_view()),
+    path('get_accounts/', account_views.GetAccount.as_view()),
+    path('add_account/', account_views.AddAccount.as_view()),
+    path('get_firms/', account_views.GetFirm.as_view()),
+    path('get_account_detail/', account_views.GetAccountDetail.as_view()),
+    path('import_host/', resouce_views.ImportHost.as_view()),
+    path('get_family_firms/', resouce_views.GetFamilyFirm.as_view()),
+    path('get_hosts/', resouce_views.GetHost.as_view()),
+    path('stop_instance/', resouce_views.StopInstance.as_view()),
+    path('start_instance/', resouce_views.StartInstance.as_view()),
+    path('get_disks/', resouce_views.GetDisk.as_view()),
+    path('get_snapshots/', resouce_views.GetSnapshot.as_view()),
 ]
